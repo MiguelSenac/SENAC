e3dd7ade7c39dc3ad329fe182e4ce3969e6f0dee
TollRent <miguel.senacrs@gmail.com>
2024-09-26 19:35:44 -0300

    Delete algoritmos directory

--- algoritmos/parImpar.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-const num = prompt("Digite um número: ")
-if (num % 2 == 0)
-    console.log("O número " + num + " é par" )
-else
-    console.log("O número " + num + " é ímpar")
--- algoritmos/teste.js
+++
@@ -1,4 +0,0 @@
-const prompt = require("prompt-sync")();
-
-nome = prompt("Digite seu nome: ")
-console.log("Bem vindo " + nome )
